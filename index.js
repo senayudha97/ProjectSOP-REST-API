c9599130f8520190cde9492cb42453be64bb8217
Sena Yudha <senayudha97@outlook.com>
2021-06-17 01:08:26 +0700

    update all fomr serve Rest API

--- index.js
+++ index.js
@@ -36,12 +36,14 @@ app.get("/data", async (req, res) => {
   }
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   try {
-    knex("log_input")
+    let id = await knex("log_input")
       .insert(req.body)
-      .then(function () {
-        res.send("BERHASIL");
+      .then(function (id) {
+        res.json({
+          id: id[0],
+        });
       })
       .catch(function (error) {
         res.send(error);
@@ -52,9 +54,122 @@ app.post("/add", (req, res) => {
   }
 });
 
+app.post("/sop_infus", async (req, res) => {
+  try {
+    let dataPost = req.body;
+    let id = dataPost.id;
+    let data = dataPost.data;
+
+    await knex("log_input")
+      .where("id", id)
+      .update({ jwb_pemasangan_infus: data })
+      .then(function () {
+        res.send(true);
+      })
+      .catch(function (error) {
+        res.send(error);
+      });
+    res.send(dataPost.id);
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
+
+app.post("/sop_luka", async (req, res) => {
+  try {
+    let dataPost = req.body;
+    let id = dataPost.id;
+    let data = dataPost.data;
+
+    await knex("log_input")
+      .where("id", id)
+      .update({ jwb_perawatan_luka: data })
+      .then(function () {
+        res.send(true);
+      })
+      .catch(function (error) {
+        res.send(error);
+      });
+    res.send(dataPost.id);
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
+
+app.post("/sop_oksigen", async (req, res) => {
+  try {
+    let dataPost = req.body;
+    let id = dataPost.id;
+    let data = dataPost.data;
+
+    await knex("log_input")
+      .where("id", id)
+      .update({ jwb_memberikan_oksigen: data })
+      .then(function () {
+        res.send(true);
+      })
+      .catch(function (error) {
+        res.send(error);
+      });
+    res.send(dataPost.id);
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
+
+app.post("/sop_luka_bakar", async (req, res) => {
+  try {
+    let dataPost = req.body;
+    let id = dataPost.id;
+    let data = dataPost.data;
+
+    await knex("log_input")
+      .where("id", id)
+      .update({ jwb_penanganan_luka_bakar: data })
+      .then(function () {
+        res.send(true);
+      })
+      .catch(function (error) {
+        res.send(error);
+      });
+    res.send(dataPost.id);
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
+
+app.post("/sop_observasi", async (req, res) => {
+  try {
+    let dataPost = req.body;
+    let id = dataPost.id;
+    let data = dataPost.data;
+
+    await knex("log_input")
+      .where("id", id)
+      .update({ jwb_pasien_gawat: data })
+      .then(function () {
+        res.send(true);
+      })
+      .catch(function (error) {
+        res.send(error);
+      });
+    res.send(dataPost.id);
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
+
 // Set Port
-app.listen(3000, "192.168.1.11", function () {
+app.listen(3000, function () {
   console.log("Node app is running on port 3000");
 });
+// app.listen(3000, "192.168.1.27", function () {
+//   console.log("Node app is running on port 3000");
+// });
 
 module.exports;
